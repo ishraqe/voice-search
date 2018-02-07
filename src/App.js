287b43b444a1e58895c67b53a9839ba919bb56de
ishraqe <ishraqenonymous@gmail.com>
2018-02-07 17:01:13 +0600

    half doone

---
+++ src/App.js
@@ -0,0 +1,202 @@
+import React, {Component} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  AppRegistry,
+  TouchableHighlight} from 'react-native';
+import Voice from 'react-native-voice';
+import tts from 'react-native-android-speech';
+import axios from 'axios';
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      recognized: '',
+      pitch: '',
+      error: '',
+      end: '',
+      started: '',
+      results: [],
+    };
+    Voice.onSpeechStart = this.onSpeechStart.bind(this);
+    Voice.onSpeechRecognized = this.onSpeechRecognized.bind(this);
+    Voice.onSpeechEnd = this.onSpeechEnd.bind(this);
+    Voice.onSpeechError = this.onSpeechError.bind(this);
+    Voice.onSpeechResults = this.onSpeechResults.bind(this);
+  }
+
+  componentWillUnmount() {
+    Voice.destroy().then(Voice.removeAllListeners);
+  }
+
+  onSpeechStart(e) {
+    this.setState({
+      started: '√',
+    });
+  }
+
+  onSpeechRecognized(e) {
+    this.setState({
+      recognized: '√',
+    });
+  }
+
+  onSpeechEnd(e) {
+    this.setState({
+      end: '√',
+    });
+  }
+
+  onSpeechError(e) {
+    this.setState({
+      error: JSON.stringify(e.error),
+    });
+  }
+
+  onSpeechResults(e) {
+    this.setState({
+      results: e.value,
+    });
+  }
+
+  async _startRecognizing(e) {
+    this.setState({
+      recognized: '',
+      pitch: '',
+      error: '',
+      started: '',
+      results: [],
+      partialResults: [],
+      end: ''
+    });
+    try {
+      await Voice.start('en-US');
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  async _stopRecognizing(e) {
+    try {
+      await Voice.stop();
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  async _cancelRecognizing(e) {
+    try {
+      await Voice.cancel();
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  async _destroyRecognizer(e) {
+    try {
+      await Voice.destroy();
+    } catch (e) {
+      console.error(e);
+    }
+    this.setState({
+      recognized: '',
+      pitch: '',
+      error: '',
+      started: '',
+      results: [],
+      end: ''
+    });
+  }
+
+  fetchResult = () => {
+    if (this.state.results.length > 0) {
+      axios.get('https://en.wikipedia.org/wiki/' + this.state.results[0])
+        .then(function (response) {
+          console.log(response.data);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+  }
+
+  render() {
+    
+   
+    return (
+      <View style={styles.container}>
+       
+        <Text style={styles.instructions}>
+          Press the button and start speaking.
+        </Text>
+       
+        <Text style={styles.stat}> {this.state.results[0]} </Text>
+        
+        <TouchableHighlight onPress={this._startRecognizing.bind(this)}>
+          <Text style={styles.action}> 
+            Start
+          </Text>
+        </TouchableHighlight>
+        <TouchableHighlight onPress={this._stopRecognizing.bind(this)}>
+          <Text
+            style={styles.action}>
+            Stop Recognizing
+          </Text>
+        </TouchableHighlight>
+        <TouchableHighlight onPress={this._cancelRecognizing.bind(this)}>
+          <Text
+            style={styles.action}>
+            Cancel
+          </Text>
+        </TouchableHighlight>
+        <TouchableHighlight onPress={this._destroyRecognizer.bind(this)}>
+          <Text
+            style={styles.action}>
+            Destroy
+          </Text>
+        </TouchableHighlight>
+        {this.fetchResult()}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  button: {
+    width: 50,
+    height: 50,
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  action: {
+    textAlign: 'center',
+    color: '#0000FF',
+    marginVertical: 5,
+    fontWeight: 'bold',
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  stat: {
+    textAlign: 'center',
+    color: '#B0171F',
+    marginBottom: 1,
+  },
+});
+
+
+export default App;
